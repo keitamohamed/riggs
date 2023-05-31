62133f1883434934494b4f73441f1504b0c80c25
mohamedkeita <keitamohamed12@gmail.com>
2023-05-31 08:06:56 -0400

    Can create booking, delete, find by id, get all booking

--- src/main/java/com/keita/riggs/controller/BookingController.java
+++ src/main/java/com/keita/riggs/controller/BookingController.java
@@ -1,11 +1,64 @@
 package com.keita.riggs.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.keita.riggs.model.Booking;
+import com.keita.riggs.service.BookingService;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/riggs/booking")
 public class BookingController {
+
+    private final BookingService bookingService;
+
+    @Autowired
+    public BookingController(BookingService bookingService) {
+        this.bookingService = bookingService;
+    }
+
+    @PostMapping(
+            path = {"/add"},
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> newBooking(
+            @Valid
+            @RequestBody
+            Booking booking,
+            BindingResult bindingResult
+    ) {
+        return bookingService.save(booking, bindingResult);
+    }
+
+    @GetMapping(path = {"/find-by-id/{bookingID}"})
+    public Optional<Booking> findBookingByID(
+            @PathVariable
+            Long bookingID,
+            HttpServletResponse servletResponse
+    ) {
+        return bookingService.findBookingByID(bookingID, servletResponse);
+    }
+
+    @GetMapping(path = {"/list-of-booking"})
+    public List<Booking> bookingList(HttpServletResponse servletResponse) {
+        return bookingService.bookingList(servletResponse);
+    }
+
+    @DeleteMapping(
+            path = {"/delete-booking/{bookingID}"}
+    )
+    public ResponseEntity<?> deleteBooking(
+            @PathVariable
+            Long bookingID
+    ) {
+        return bookingService.deleteBooking(bookingID);
+    }
 }
